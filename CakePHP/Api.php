5e1d13af571c159dfe5b8a57d09f76d0b9dcd8b3
Nelson Martell <nelson6e65-dev@yahoo.es>
2014-12-19 10:33:24 -0430

    add: Class CakePHP.Api

---
+++ CakePHP/Api.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Clase para manejar los directorios de las diferentes APIs. CAkePHP
+ * */
+public class Api {
+	
+	public function __construct($apiName, $apiVersion) {
+		$this->name = $apiName;
+		$this->version = $apiVersion;
+	}
+	
+	public $name = '';
+	
+	public $version = '';
+	
+	/*
+		public static function getJavascriptPath($api) {
+		
+		}
+		
+		public static function getCssPath($api) {
+		
+		}
+		
+		public static function getImgPath($api){
+		
+		}
+		
+		public static function getResourcesPath($api){
+		
+	}*/
+	
+	/*
+	 * Obtiene la ruta relativa usando algún auxiliar, como Html->css(), por ejemplo.
+	 * */
+	public function getPath(boolean $endWithName = false, string $append = ''){
+		
+		$ruta = $this->name . '/' . $this->version . '/';
+		
+		if ($endWithName)
+			$ruta .= $this->name;
+		
+		
+		
+		if ($append != '')
+		$ruta .= $append; //Le adiciona $append al final de la cadena
+		
+		$ruta = strtolower($ruta); //Conversión a minúsculas
+		
+		$ruta = str_replace(' ', '-', $ruta);		
+		
+		return $ruta;
+	}	
+}
