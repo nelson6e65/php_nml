5e0b7af44e13b4cc111e933fed0148327db26840
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-01-06 04:41:15 -0430

    fix: Redefined: Type now extends Object

--- Type.php
+++ Type.php
@@ -18,44 +18,120 @@
 if (!defined("C_Type")) {		
 	define("C_Type", true);
 
+	include('Object.php');
 	
 	
-	final class Type {
+	final class Type extends Object {
+		
+		function __construct($obj){
+			parent::__construct();
+			unset($this->Name, $this->Vars, $this->Methods);
+			
+			$name = gettype($obj);
+			$vars = array();
+			$methods = array();
+			
+			if ($name == 'object') {
+				$name = get_class($obj);
+				if ($name == 'Type') {
+					$vars = $obj->Vars;
+					$methods = $obj->Methods;
+				} else {
+					$vars = get_class_vars($name);
+					$methods = get_class_methods($obj);
+				}
+				
+			} else {
+				$name = $o;
+			}
+			
+			$this->_name = $name;
+			$this->_vars = $vars;
+			$this->_methods = $methods;
+		}
+		
+		
+		private $_name;
 		public $Name;
 		
-		function __construct($nombre){
-			if (is_string($nombre)) {
-				$this->Name = $nombre;
-			} else {
-				$this->InicializeFromType(typeof($nombre));
-			}
+		/*
+		 * 
+		 * @return  string  Nombre del tipo
+		 * */
+		public function get_Name() {
+			return $this->_name;
 		}
 		
-		///Obtiene el nombre del tipo
+		/*
+		 * 
+		 * @deprecated Use Name property instead
+		 * */
 		public function GetName() {
-			return $this->Name;
+			return $this->get_Name();
 		}
 		
+		private $_vars;
 		public $Vars;
-		public function GetVars() {
-			return $this->Vars;
+		
+		public function get_Vars() {
+			return $this->_vars;
 		}
-				
+		
+		
+		/*
+		 * @deprecated Use Vars property instead
+		 * */
+		public function GetVars() {
+			return $this->get_Vars();
+		}
+		
+		
+		private $_methods;
 		public $Methods;
+		
+		public function get_Methods(){
+			return $this->_methods;
+		}
+		
+		/*
+		 * 
+		 * @deprecated Use Methods property instead
+		 * */
 		public function GetMethods() {
-			return $this->Methods;
+			return $this->get_Methods();
 		}
 
 		
-
+		/*
 		//Genera un Type a partir de otro.
 		public function InicializeFromType($type) {
 			$this->Name = $type->Name;
 			$this->Methods = $type->Methods;
 		}
-
+		*/
+		
+		private static function _isCustom($s) {
+			$s = (string) $s;
+			
+			switch($s){
+				case 'string':
+				case 'integer':
+				case 'double':
+				case 'boolean':
+				case 'array':
+				case 'object':
+				case 'NULL':
+				case 'null':
+					return false;
+				default:
+					return true;
+			}
+		}
+		
 		//Indica si el tipo especificado es una clase definida; es decir, diferente a 'integer', 'double', 'array' y 'object'.
 		public function IsCustom() {
+			
+
 			switch($this->Name){
 				case 'string':
 				case 'integer':
@@ -76,6 +152,7 @@ if (!defined("C_Type")) {
 				case 'integer':
 				case 'double':
 				case 'array':
+				case 'boolean':
 					return true;
 				default:
 					return false;
@@ -94,28 +171,25 @@ if (!defined("C_Type")) {
 			}
 		}
 
-		public final function __toString() {
+		public function ToString() {
 			$aux = "t";
 			if ($this->IsCustom())
 				$aux = "T";
 			return $aux . ":" . $this->Name;
 		}
+		
+		
+		 
 	}
 	
 	/*
-	 * Obtiene el Type de un objeto
+	 * Obtiene el Type de un objeto. Es un alias para el constructor de Type.
+	 * 
 	 * @param  mixed Objeto a extraer su tipo
 	 * @return  Type Tipo del objeto
 	 * */
 	function typeof($obj) {
-		$t = new Type(gettype($obj));				
-		
-		if ($t->Name == 'object') {
-			$t = new Type(get_class($obj));
-			$t->Methods = get_class_methods($obj);
-		}
-		
-		return $t;
+		return new Type($obj);
 	}
 	
 	
