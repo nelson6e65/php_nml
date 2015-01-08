81d815abc4d3ef5c3a807ae5b6d7ed547d5cb82d
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-01-07 23:48:36 -0430

    fix: Now, allowed empty Assert constructor | add: ShortName and CdnUri properties

--- Cake/Asset.php
+++ Cake/Asset.php
@@ -13,55 +13,111 @@ if (!defined($_namespace . '/' . $_class)):
 	include('/../Object.php');
 	
 	class Asset extends Object {
-		
-		public function __construct($name, $versions) {
+		/* *
+		 * Crea una nueva instancia de la clase Asset
+		 * 
+		 * 
+		 * 
+		 * */
+		public function __construct($name = '', $versions = null, $cdnUri = null) {
 			parent::__construct();
-			unset($this->Name, $this->Versions);
+			unset($this->Name, $this->Versions, $this->ShortName, $this->CdnUri);
 			
-			if (!is_string($name))
-				throw new InvalidArgumentException(_('The name of resource must be an string.'));
-			
-			$this->Name = $name;
-			
-			if(is_array($versions))
-				$this->Versions = $versions;
+			if ($name == '') {
+				if ($versions != null) {
+					throw new InvalidArgumentException(_('The $name of resource can not be null.'));
+				}
+			} else {
+				$this->_name = $name;
 				
+				if (is_array($versions)) {
+					$this->_versions = $versions;
+				}
+			
+			}
 			
 		}
 		
-		private $_name;
 		
 		/*
-		 * Obtiene o establece el nombre del recurso.
+		 * Obtiene o establece el nombre original del recurso.
+		 * 
+		 * @var  string Nombre del recurso
 		 * */
 		public $Name;
+		private $_name;
 		
 		public function get_Name() {
 			return $this->_name;
 		}
 		
 		public function set_Name($value) {
-			if(!is_string($value))
-				throw new InvalidArgumentException('$value must be an string');
+			if (!is_string($value)) {
+				throw new InvalidArgumentException('$value argument must be string.');
+			}
 			
-			$this->_name = $value;
+			if (str_word_count($value) == 0) {
+				throw new InvalidArgumentException('$value argument can not be an empty or whitespace string.');
+			}
+			
+			$this->_name = trim($value);
+			
+			$this->_shortName = str_replace(' ', '-', strtolower($this->_name));
 		}
 		
-		private $_versions;
+		/*
+		 * Obtiene el nombre auto-generado del recurso.
+		 * 
+		 * @var  string Nombre del recurso en su forma generada
+		 * */
+		public $ShortName;
+		private $_shortName;
+		
+		public function get_ShortName() {
+			return $this->_shortName;
+		}
 		
 		/*
-		 * Obtiene o establece la lista de versiones
+		 * Obtiene la lista de versiones
+		 * 
+		 * @var  List Lista de versiones del recurso
 		 * */
 		public $Versions;
+		private $_versions;
 		
 		public function get_Versions() {
 			return $this->_versions;
 		}
-		
+		/*
 		public function set_Versions($value) {
 			$this->_versions = $value;
 		}
+		*/
 		
+		
+		/* *
+		 * Obtiene o establece el CDN del recurso.
+		 * Debe modificarse la URL, colocando '{v}' en vez de la versión.
+		 * @example  Un CDN para el JavaScript de jQuery UI v1.11.2 es:
+		 *   "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"
+		 *   Entonces el valor de esta propiedad debe ser:
+		 *   "//ajax.googleapis.com/ajax/libs/jqueryui/{v}/jquery-ui"
+		 * 
+		 * 
+		 * 
+		 * @var  string CDN
+		 * */
+		public $CdnUri;
+		private $_cdnUri;
+		
+		public function get_CdnUri() {
+			return $this->_cdnUri;
+		}
+		
+		public function set_CdnUri($value) {
+			$this->_cdnUri = (string) $value;
+		}
+	
 		/*
 			public static function getJavascriptPath($api) {
 			
