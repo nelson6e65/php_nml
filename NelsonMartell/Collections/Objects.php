760605963562d862c9b50ecec519f30f34295dd9
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-01-13 00:27:52 -0430

    add: [NelsonMartell.Collections.] Objects class.

---
+++ NelsonMartell/Collections/Objects.php
@@ -0,0 +1,124 @@
+﻿<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * PHP class «Objects»
+ *
+ * Copyright © 2015 Nelson Martell (http://fb.me/nelson6e65)
+ *
+ * Licensed under The MIT License (MIT)
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright	Copyright © 2015 Nelson Martell
+ * @link		https://github.com/nelson6e65/NelsonMartell
+ * @package  	NelsonMartell.Collections
+ * @license  	http://www.opensource.org/licenses/mit-license.php The MIT License (MIT)
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+$_namespace = "NelsonMartell";
+$_class = "Objects";
+
+if (!defined($_namespace . '/' . $_class)):
+	define($_namespace . '/' . $_class, true);
+
+	include('/../Object.php');
+
+
+	/* *
+	 * Clase base de una colección de objetos, que define métodos y propiedades básicas para
+	 * trabajar con colecciones.
+	 *
+	 *
+	 * @package  NelsonMartell.Collections
+	 * @author   Nelson Martell (@yahoo.es: nelson6e65-dev)
+	 * */
+	abstract class Objects
+		extends Object {
+
+
+		function __construct() {
+			parent::__construct();
+			unset($this->Count);
+		}
+
+		private $_items = array();
+
+		public $Count;
+		private $_count = 0;
+
+		public function get_Count() {
+			return $this->_count;
+		}
+
+
+
+
+		public final function __invoke($index, $value = null) {
+			if ($value == null) {
+				return $this->_items[$index];
+			}
+
+			$this->SetItem($index, $value);
+		}
+
+		protected function InsertItem($index, $newItem) {
+
+			if ($index > $this->Count || $index < 0) {
+				throw new OutOfRangeException();
+			}
+
+			if ($index == $this->Count){
+				$this->_items[$index] = null;
+				$this->_count++;
+			}
+
+			$this->_items[$index] = $newItem;
+		}
+
+		protected function SetItem($index, $newItem) {
+
+			if ($index >= $this->Count || $index < 0) {
+				throw new OutOfRangeException();
+			}
+
+			$this->_items[$index] = $newItem;
+		}
+
+		protected function RemoveItem($index) {
+			if ($index >= $this->Count || $index < 0) {
+				throw new OutOfRangeException();
+			}
+
+			for($i = $index; $i < $this->Count - 1; $i++) {
+				$this->_items[$i] = $this->_items[$i + 1]; //Mueve los valores
+			}
+
+			unset($this->_items[$this->Count - 1]); //Des-asigna el último elemento
+
+			$this->_count--;
+		}
+
+		protected function ClearItems() {
+			$this->_items = new array();
+			$this->_count = 0;
+		}
+
+
+		public function ToString() {
+			$t = $this->GetType();
+
+			$s = $t->Name . ': { ';
+
+			foreach ($this->_items as $item) {
+				$s .= $item . ', ';
+			}
+
+			$s .= '}';
+
+			return $s;
+
+		}
+
+	}
+
+endif;
