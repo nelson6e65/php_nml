2586c24211d1efb4d30c1abd609d6e2a2eb138df
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-05-23 03:49:41 -0430

    add: PSR2 corrections to `IComparable`.

--- src/IComparable.php
+++ src/IComparable.php
@@ -19,31 +19,32 @@
 
 namespace NelsonMartell {
 
-	/**
-	 * Provee un método para compara igualdad entre objetos del mismo tipo.
-	 *
-	 * @author  Nelson Martell (nelson6e65-dev@yahoo.es)
-	 * */
-	interface IComparable  {
+    /**
+     * Provee un método para compara igualdad entre objetos del mismo tipo.
+     *
+     * @author  Nelson Martell (nelson6e65-dev@yahoo.es)
+     * */
+    interface IComparable
+    {
 
-		/**
-		 * Determina la posición relativa del objeto especificado con respecto a esta instancia.
-		 *
-		 *
-		 * @param   mixed    $other
-		 * @return  integer  0, si es igual; >0, si es mayor; <0, si es menor.
-		 * */
-		public function CompareTo($other);
+        /**
+         * Determina la posición relativa del objeto especificado con respecto a esta instancia.
+         *
+         *
+         * @param   mixed    $other
+         * @return  integer  0, si es igual; >0, si es mayor; <0, si es menor.
+         * */
+        public function compareTo($other);
 
-		/**
-		 * Determina la posición relativa del objeto de la derecha con respecto al de la izquierda.
-		 * Puede usarse como segundo argumento en la función de ordenamiento de arrays 'usort'.
-		 *
-		 *
-		 * @param   mixed    $left  Objeto de la izquierda
-		 * @param   mixed    $right  Objeto de la derecha
-		 * @return  integer  0, si ambos son iguales; >0, si $right es mayor a $left; <0, si $left es mayor a $right.
-		 * */
-		public static function Compare($left, $right);
-	}
+        /**
+         * Determina la posición relativa del objeto de la derecha con respecto al de la izquierda.
+         * Puede usarse como segundo argumento en la función de ordenamiento de arrays 'usort'.
+         *
+         *
+         * @param   mixed    $left  Objeto de la izquierda
+         * @param   mixed    $right  Objeto de la derecha
+         * @return  integer  0, si ambos son iguales; >0, si $right es mayor a $left; <0, si $left es mayor a $right.
+         * */
+        public static function compare($left, $right);
+    }
 }
