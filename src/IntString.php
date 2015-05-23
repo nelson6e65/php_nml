6dbe072f35b20208fc1e1bb20a1aadae03ffc9b1
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-05-23 05:30:22 -0430

    Applied PSR2 corrections to `IntString` class (incomplete). Tab to spaces.

--- src/IntString.php
+++ src/IntString.php
@@ -19,116 +19,116 @@
 
 namespace NelsonMartell {
 
-	/**
-	 * Representa un elemento mixto, compuesto por un entero y una cadena unidos (en ese orden).
-	 * El método ToString obtiene esa cadena compuesta.
-	 *
-	 *
-	 * @author  Nelson Martell (nelson6e65-dev@yahoo.es)
-	 * */
-	class IntString extends Object implements IEquatable, IComparable
-	{
+    /**
+     * Representa un elemento mixto, compuesto por un entero y una cadena unidos (en ese orden).
+     * El método ToString obtiene esa cadena compuesta.
+     *
+     *
+     * @author  Nelson Martell (nelson6e65-dev@yahoo.es)
+     * */
+    class IntString extends Object implements IEquatable, IComparable
+    {
 
-		public function __construct($intValue = 0, $stringValue = '')
-		{
-			unset($this->IntValue, $this->StringValue);
+        public function __construct($intValue = 0, $stringValue = '')
+        {
+            unset($this->IntValue, $this->StringValue);
 
-			if (is_integer($intValue) or $intValue == null) {
-				$this->_intValue = $intValue;
-			} else {
-				//Try convert to integer
-				$this->_intValue = (integer) $intValue;
-			}
+            if (is_integer($intValue) or $intValue == null) {
+                $this->_intValue = $intValue;
+            } else {
+                //Try convert to integer
+                $this->_intValue = (integer) $intValue;
+            }
 
-			$this->_stringValue = (string) $stringValue;
-		}
+            $this->_stringValue = (string) $stringValue;
+        }
 
-		public static function parse($value)
-		{
-			if ($value instanceof IntString) {
-				return $value;
-			}
+        public static function parse($value)
+        {
+            if ($value instanceof IntString) {
+                return $value;
+            }
 
-			$s = (string) $value;
-			$intValue = (int) $s;
+            $s = (string) $value;
+            $intValue = (int) $s;
 
-			$stringValue = explode($intValue, $s, 2);
+            $stringValue = explode($intValue, $s, 2);
 
-			if ($intValue > 0 or $stringValue[1] != '') {
-				$stringValue = $stringValue[1];
-			} else {
-				$stringValue = $stringValue[0];
-			}
+            if ($intValue > 0 or $stringValue[1] != '') {
+                $stringValue = $stringValue[1];
+            } else {
+                $stringValue = $stringValue[0];
+            }
 
-			return new IntString($intValue, $stringValue);
-		}
-
-
-		protected $_intValue;
-		protected $_stringValue;
-
-		public $IntValue;
-		public function get_IntValue()
-		{
-			return (int) $this->_intValue;
-		}
-
-		public $StringValue;
-		public function get_StringValue()
-		{
-			return $this->_stringValue;
-		}
-
-		public function toString()
-		{
-			return $this->IntValue . $this->StringValue;
-		}
-
-		public function equals($other)
-		{
-			if ($other instanceof IntString) {
-				if ($this->IntValue === $other->IntValue) {
-					if ($this->StringValue === $other->StringValue) {
-						return true;
-					}
-				}
-			}
-
-			return false;
-		}
+            return new IntString($intValue, $stringValue);
+        }
 
 
-		#region IComparable
+        protected $_intValue;
+        protected $_stringValue;
 
-		/**
-		 * Determina la posición relativa de esta instancia con respecto al objeto especificado.
-		 * Nota: Cualquier objeto que no sea instancia de IntString se considerará menor.
-		 *
-		 *
-		 * @param  IntString|mixed  $other  Objeto con el que se va a comparar.
-		 * @return  integer  Cero (0), si esta instancia es igual a $other; mayor a cero (>0),
-		 *     si es mayor a $other; menor a cero (<0), si es menor.
-		 * */
-		public function compareTo($other)
-		{
+        public $IntValue;
+        public function get_IntValue()
+        {
+            return (int) $this->_intValue;
+        }
 
-			$r = $this->Equals($other) ? 0 : 9999;
+        public $StringValue;
+        public function get_StringValue()
+        {
+            return $this->_stringValue;
+        }
 
-			if ($r != 0) {
-				if ($other instanceof IntString) {
-					$r = $this->IntValue - $other->IntValue;
+        public function toString()
+        {
+            return $this->IntValue . $this->StringValue;
+        }
 
-					if ($r == 0) {
-						$r = $this->StringValue < $other->StringValue ? -1 : 1;
-					}
-				} else {
-					$r = 1;
-				}
-			}
+        public function equals($other)
+        {
+            if ($other instanceof IntString) {
+                if ($this->IntValue === $other->IntValue) {
+                    if ($this->StringValue === $other->StringValue) {
+                        return true;
+                    }
+                }
+            }
 
-			return $r;
-		}
+            return false;
+        }
 
-		#endregion
-	}
+
+        #region IComparable
+
+        /**
+         * Determina la posición relativa de esta instancia con respecto al objeto especificado.
+         * Nota: Cualquier objeto que no sea instancia de IntString se considerará menor.
+         *
+         *
+         * @param  IntString|mixed  $other  Objeto con el que se va a comparar.
+         * @return  integer  Cero (0), si esta instancia es igual a $other; mayor a cero (>0),
+         *     si es mayor a $other; menor a cero (<0), si es menor.
+         * */
+        public function compareTo($other)
+        {
+
+            $r = $this->Equals($other) ? 0 : 9999;
+
+            if ($r != 0) {
+                if ($other instanceof IntString) {
+                    $r = $this->IntValue - $other->IntValue;
+
+                    if ($r == 0) {
+                        $r = $this->StringValue < $other->StringValue ? -1 : 1;
+                    }
+                } else {
+                    $r = 1;
+                }
+            }
+
+            return $r;
+        }
+
+        #endregion
+    }
 }
