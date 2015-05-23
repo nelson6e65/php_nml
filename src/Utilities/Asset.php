48312aff78c0c59f719288bb2e1a76baab4896fb
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-05-23 07:00:13 -0430

    Applied PSR2 corrections to Asset class (incomplete). Missing: 7 ERRORS AND 4 WARNINGS AFFECTING 11 LINES ----------------------------------------------------------------------  117 | WARNING | Property name "$_name" should not be prefixed with      |         | an underscore to indicate visibility  119 | ERROR   | Method name "Asset::get_Name" is not in camel caps      |         | format  124 | ERROR   | Method name "Asset::set_Name" is not in camel caps      |         | format  145 | WARNING | Property name "$_shortName" should not be prefixed      |         | with an underscore to indicate visibility  147 | ERROR   | Method name "Asset::get_ShortName" is not in camel      |         | caps format  158 | WARNING | Property name "$_versions" should not be prefixed      |         | with an underscore to indicate visibility  160 | ERROR   | Method name "Asset::get_Versions" is not in camel      |         | caps format  177 | WARNING | Property name "$_cdnUri" should not be prefixed with      |         | an underscore to indicate visibility  179 | ERROR   | Method name "Asset::get_CdnUri" is not in camel caps      |         | format  184 | ERROR   | Method name "Asset::set_CdnUri" is not in camel caps      |         | format  196 | ERROR   | Method name "Asset::get_RootDirectory" is not in      |         | camel caps format

--- src/Utilities/Asset.php
+++ src/Utilities/Asset.php
@@ -11,13 +11,14 @@
  * For full copyright and license information, please see the LICENSE
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright  Copyright © 2014, 2015 Nelson Martell
- * @link       http://nelson6e65.github.io/php_nml/
- * @since      v0.1.1
- * @license    http://www.opensource.org/licenses/mit-license.php The MIT License (MIT)
+ * @copyright Copyright © 2014, 2015 Nelson Martell
+ * @link      http://nelson6e65.github.io/php_nml/
+ * @since     v0.1.1
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License (MIT)
  * */
 
 namespace NelsonMartell\Utilities {
+
     use NelsonMartell\Object;
     use NelsonMartell\Version;
     use \InvalidArgumentException;
@@ -28,17 +29,15 @@ namespace NelsonMartell\Utilities {
      * Contiene métodos y propiedades para obtener las rutas de los directorios y recursos de las
      * diferentes versiones del framework.
      *
-     *
-     * @author  Nelson Martell (nelson6e65-dev@yahoo.es)
+     * @author Nelson Martell <nelson6e65-dev@yahoo.es>
      * */
-    class Asset extends Object {
+    class Asset extends Object
+    {
         /**
          * Crea una nueva instancia de la clase Asset
-         *
-         *
-         *
          * */
-        public function __construct($name = null, $versions = null, $cdnUri = null) {
+        public function __construct($name = null, $versions = null, $cdnUri = null)
+        {
             parent::__construct();
             unset($this->Name, $this->Versions, $this->ShortName, $this->CdnUri, $this->RootDirectory);
 
@@ -50,7 +49,9 @@ namespace NelsonMartell\Utilities {
             }
 
             if ($this->Name == '' && $versions != null) {
-                throw new InvalidArgumentException(dgettext('nml', 'Can not specify $versions argument if $name argument is null.'));
+                throw new InvalidArgumentException(
+                    dgettext('nml', 'Can not specify $versions argument if $name argument is null.')
+                );
             }
 
             if ($versions == null) {
@@ -58,18 +59,22 @@ namespace NelsonMartell\Utilities {
             }
 
             if (is_array($versions)) {
-
                 $this->_versions = array();
 
                 if (count($versions) > 0) {
                     $i = 0;
-                    foreach($versions as $version) {
+                    foreach ($versions as $version) {
                         $v = $version;
                         if (!($v instanceof Version)) {
                             try {
                                 $v = Version::Parse($version);
                             } catch (InvalidArgumentException $e) {
-                                throw new InvalidArgumentException('$versions argument must be an array of Version objects or any objects parseable into Version.', 0, $e);
+                                throw new InvalidArgumentException(
+                                    '$versions argument must be an array of Version objects ' .
+                                    'or any objects parseable into Version.',
+                                    0,
+                                    $e
+                                );
                             }
                         }
 
@@ -84,7 +89,12 @@ namespace NelsonMartell\Utilities {
                 try {
                     $v = Version::Parse($versions);
                 } catch (InvalidArgumentException $e) {
-                    throw new InvalidArgumentException('$versions argument must be an array of Version objects (or empty), a Version object or any object parseable into Version.', 0, $e);
+                    throw new InvalidArgumentException(
+                        '$versions argument must be an array of Version objects (or empty), a Version object ' .
+                        'or any object parseable into Version.',
+                        0,
+                        $e
+                    );
                 }
 
                 $this->_versions = array($v);
@@ -100,18 +110,19 @@ namespace NelsonMartell\Utilities {
          * será éste mismo pero convertido en minúsculas y reemplazando sus espacios en blanco por
          * guiones (' ' -> '-')).
          *
-         *
-         * @see  $ShortName
-         * @var  string Nombre del recurso
+         * @see $ShortName
+         * @var string Nombre del recurso
          * */
         public $Name;
         private $_name;
 
-        public function get_Name() {
+        public function get_Name()
+        {
             return $this->_name;
         }
 
-        public function set_Name($value) {
+        public function set_Name($value)
+        {
             if (!is_string($value)) {
                 throw new InvalidArgumentException('$value argument must be string.');
             }
@@ -128,48 +139,50 @@ namespace NelsonMartell\Utilities {
         /**
          * Obtiene el nombre real del recurso, que representa al nombre real de .
          *
-         *
-         * @var  string Nombre del recurso en su forma generada
+         * @var string Nombre del recurso en su forma generada
          * */
         public $ShortName;
         private $_shortName;
 
-        public function get_ShortName() {
+        public function get_ShortName()
+        {
             return $this->_shortName;
         }
 
         /**
          * Obtiene la lista de versiones
          *
-         *
-         * @var  List Lista de versiones del recurso
+         * @var List Lista de versiones del recurso
          * */
         public $Versions;
         private $_versions;
 
-        public function get_Versions() {
+        public function get_Versions()
+        {
             return $this->_versions;
         }
 
         /**
          * Obtiene o establece el CDN del recurso.
          * Debe modificarse la URL, colocando '{v}' en vez de la versión.
-         * @example  Un CDN para el JavaScript de jQuery UI v1.11.2 es:
+         *
+         * @example Un CDN para el JavaScript de jQuery UI v1.11.2 es:
          *   "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"
          *   Entonces el valor de esta propiedad debe ser:
          *   "//ajax.googleapis.com/ajax/libs/jqueryui/{v}/jquery-ui"
          *
-         *
-         * @var  string CDN
+         * @var string CDN
          * */
         public $CdnUri;
         private $_cdnUri;
 
-        public function get_CdnUri() {
+        public function get_CdnUri()
+        {
             return $this->_cdnUri;
         }
 
-        public function set_CdnUri($value) {
+        public function set_CdnUri($value)
+        {
             $this->_cdnUri = (string) $value;
         }
 
@@ -177,11 +190,11 @@ namespace NelsonMartell\Utilities {
         /**
          * Obtiene el directorio principal del recurso.
          *
-         *
-         * @var  string Ruta inicial del recurso
+         * @var string Ruta inicial del recurso
          * */
         public $RootDirectory;
-        public function get_RootDirectory() {
+        public function get_RootDirectory()
+        {
             return $this->ShortName . '/';
         }
 
@@ -193,12 +206,12 @@ namespace NelsonMartell\Utilities {
          * Obtiene la ruta del directorio de la versión especificada. Si no se especifica,
          * se devuelve la versión más reciente.
          *
-         *
-         * @param  string|Version  $version  Versión a obtener. También puede tomar los valores
-         *   'newest' u 'oldest' para representar a la versión más nueva o más vieja, respectivamente.
-         * @return  string Ruta del directorio de la versión especificada.
+         * @param  string|Version $version Versión a obtener. También puede tomar los valores 'newest' u 'oldest'
+         *   para representar a la versión más nueva o más vieja, respectivamente.
+         * @return string Ruta del directorio de la versión especificada.
          * */
-        public function GetDirectoryPath($version = self::NEWEST) {
+        public function getDirectoryPath($version = self::NEWEST)
+        {
             $c = count($this->Versions);
 
             if ($c == 0) {
@@ -222,7 +235,11 @@ namespace NelsonMartell\Utilities {
                 try {
                     $v = Version::Parse($version);
                 } catch (InvalidArgumentException $e) {
-                    throw new InvalidArgumentException('$version argument must be an Version object or any object parseable into Version.', 0, $e);
+                    throw new InvalidArgumentException(
+                        '$version argument must be an Version object or any object parseable into Version.',
+                        0,
+                        $e
+                    );
                 }
 
                 if (array_search($v, $this->Versions) === false) {
@@ -238,12 +255,13 @@ namespace NelsonMartell\Utilities {
          * Obtiene la ruta del recurso de la versión especificada. Si no se especifica, se devuelve la
          * versión más reciente.
          *
-         * @param  string|Version  $version  Versión a obtener. También puede tomar los valores
-         *   'newest' u 'oldest' para representar a la versión más nueva o más vieja, respectivamente.
-         * @param  string          $append   Texto que se le anezará a la cadena de salida
-         * @return  string Ruta del recurso
+         * @param  string|Version $version Versión a obtener. También puede tomar los valores 'newest' u 'oldest' para
+         *   representar a la versión más nueva o más vieja, respectivamente.
+         * @param  string         $append  Texto que se le anezará a la cadena de salida
+         * @return string Ruta del recurso
          * */
-        public function GetResourcePath($version = self::NEWEST, $append = '') {
+        public function getResourcePath($version = self::NEWEST, $append = '')
+        {
 
             $r = sprintf('%s%s%s', $this->GetDirectoryPath($version), $this->ShortName, $append);
 
