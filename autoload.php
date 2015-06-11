06747ac8d704621110ecfd2dd1e96108e42c29d7
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-06-11 08:34:32 -0430

    Updated autoload.php
    Improved to include '*.inc' files too.

--- autoload.php
+++ autoload.php
@@ -35,13 +35,17 @@ function autoload_NML($class)
         return; // Only checks for NelsonMartell namespace.
     }
 
-    $path = sprintf('%s'.DIRECTORY_SEPARATOR.'%s.php', __DIR__, implode(DIRECTORY_SEPARATOR, $classArray));
+    $path = sprintf('%s'.DIRECTORY_SEPARATOR.'%s', __DIR__, implode(DIRECTORY_SEPARATOR, $classArray));
 
-    if (is_file($path)) {
-        require_once($path);
+    if (is_file($path.'.php')) {
+        $path .= '.php';
+    } elseif (is_file($path.'.inc')) {
+        $path .= '.inc';
     } else {
         throw new Exception(sprintf(dgettext('nml', 'Unable to auto-load "%s" class in Nelson Martell Library (NML): "%s" file was not found. You can see the API documentation (http://nelson6e65.github.io/php_nml/api) in order to check availability of all classes/namespaces in NML. Note: If you are using "NelsonMartell" as main namespace in a file that not belongs to NML, you should include it before to load "NML/autoload.php" or, using SPL autoload features, register autoload function for that class(es) using "prepend" argument for spl_autoload_register function set to TRUE.'), $class, $path));
     }
+
+    require_once($path);
 }
 
 spl_autoload_register('autoload_NML');
