2b4d70e33c4b2fcab41f1723e68625b78996db2a
Nelson Martell <nelson6e65@gmail.com>
2016-03-21 20:51:53 -0430

    fix: Type::canBeString() for null type.
    - ``null`` can also be converted to string.

--- src/Type.php
+++ src/Type.php
@@ -195,7 +195,7 @@ namespace NelsonMartell {
          */
         public function canBeString()
         {
-            if ($this->isScalar() || $this->hasMethod('__toString')) {
+            if ($this->isNull() || $this->isScalar() || $this->hasMethod('__toString')) {
                 return true;
             }
 
