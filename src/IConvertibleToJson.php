28afab383426ede5feb2b91e4f055a2532e2a573
Nelson Martell <nelson6e65@gmail.com>
2016-11-07 21:58:01 -0400

    :new: Interfaces: convertible to string & JSON.
    - :new: IConvertibleToString
    - :new: IConvertibleToJson

---
+++ src/IConvertibleToJson.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PHP: Nelson Martell Library file
+ *
+ * Content:
+ * - Interface definition
+ *
+ * Copyright © 2016 Nelson Martell (http://nelson6e65.github.io)
+ *
+ * Licensed under The MIT License (MIT)
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright 2016 Nelson Martell
+ * @link      http://nelson6e65.github.io/php_nml/
+ * @since     v0.6.1
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License (MIT)
+ * */
+
+namespace NelsonMartell;
+
+use \JsonSerializable;
+
+/**
+ * Provides methods to convert objets into JSON string.
+ *
+ * @author Nelson Martell <nelson6e65@gmail.com>
+ * @since  v0.6.1
+ * */
+interface IConvertibleToJson extends IConvertibleToString, JsonSerializable
+{
+    /**
+     * Gets the JSON representation of this instance.
+     *
+     * @return string
+     */
+    public function toJson();
+}
---
+++ src/IConvertibleToString.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * PHP: Nelson Martell Library file
+ *
+ * Content:
+ * - Interface definition
+ *
+ * Copyright © 2016 Nelson Martell (http://nelson6e65.github.io)
+ *
+ * Licensed under The MIT License (MIT)
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright 2016 Nelson Martell
+ * @link      http://nelson6e65.github.io/php_nml/
+ * @since     v0.6.1
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License (MIT)
+ * */
+
+namespace NelsonMartell;
+
+/**
+ * Provides methods to convert objets into string representations.
+ *
+ * @author Nelson Martell <nelson6e65@gmail.com>
+ * @since  v0.6.1
+ * */
+interface IConvertibleToString
+{
+    /**
+     * Gets the string representation of this instance.
+     *
+     * @return string
+     */
+    public function toString();
+
+    /**
+     * Gets the implicit string representation of this instance.
+     *
+     * @return string
+     */
+    public function __toString();
+}
