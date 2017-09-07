5fa493aaa697b5eae366c99f8cef4be8978a6443
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-09-07 18:12:21 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/IntString.php
+++ src/IntString.php
@@ -32,7 +32,7 @@ use \InvalidArgumentException;
 class IntString extends Object implements IEquatable, IComparable
 {
     /**
-     * @param int|null     $intValue    Integer part. Default: ``0`` (zero).
+     * @param integer     $intValue    Integer part. Default: ``0`` (zero).
      * @param string|null  $stringValue String part. Default: ``''`` (empty).
      */
     public function __construct($intValue = 0, $stringValue = '')
