b9a89c69f5cc820d3172d2b9844cb42b70b31e39
Nelson Martell <nelson6e65@gmail.com>
2017-10-07 03:12:54 -0400

    Improve `Text` tests with dataProviders.

--- tests/TestCase/Extensions/StringTest.php
+++ tests/TestCase/Extensions/StringTest.php
@@ -3,7 +3,7 @@
  * PHP: Nelson Martell Library file
  *
  * Content:
- * - Test case for: [NelsonMartell\Extensions] String
+ * - Test case for: [NelsonMartell\Extensions] Text
  *
  * Copyright © 2016-2017 Nelson Martell (http://nelson6e65.github.io)
  *
@@ -18,13 +18,14 @@
  * */
 namespace NelsonMartell\Test\TestCase\Extensions;
 
-use NelsonMartell as NML;
 use NelsonMartell\Extensions\Text;
 use PHPUnit\Framework\TestCase;
 use \InvalidArgumentException;
+use NelsonMartell\Test\DataProviders\ExampleClass\ToString as ClassString;
+use function NelsonMartell\typeof;
 
 /**
- * Test case for `NelsonMartell\Extensions\String` class.
+ * Test case for `NelsonMartell\Extensions\Text` class.
  *
  * @see    Text
  * @author Nelson Martell <nelson6e65@gmail.com>
@@ -33,112 +34,21 @@ use \InvalidArgumentException;
  * */
 class StringTest extends TestCase
 {
-    public function testPerformsFormatForStringWithIntegerPlaceholders()
-    {
-        $expected = 'Mi nombre es Juan';
-        $actual = Text::format('Mi nombre es {0}', ['Juan']);
-        $this->assertEquals($expected, $actual);
-
-        $actual = Text::format("Mi nombre es {0}", 'Juan');
-        $this->assertEquals($expected, $actual);
-
-        $name = 'Juan';
-        $actual = Text::format("Mi nombre es {0}", $name);
-        $this->assertEquals($expected, $actual);
-
-        $expected = 'Me llamo Nelson y tengo 29 años de edad.';
-        $name = 'Nelson';
-        $age = 29;
-        $actual = Text::format('Me llamo {0} y tengo {1} años de edad.', $name, $age);
-        $this->assertEquals($expected, $actual);
-
-        $actual = Text::format('Me llamo {0} y tengo {1} años de edad.', [$name, $age]);
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function testPerformsFormatForStringsWithStringPlaceholders()
-    {
-        $expected = 'Mi nombre es Juan';
-        $actual = Text::format('Mi nombre es {name}', ["name" => "Juan"]);
-        $this->assertEquals($expected, $actual);
-
-        $expected = 'Tengo 20 años de edad.';
-        $actual = Text::format('Tengo {age} años de edad.', ["name" => "Juan", 'age' => 20]);
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function testPerformsFormatWithManyData()
-    {
-        $expected = 'Mi nombre es Juan y tengo 61 años de edad.';
-        $format   = 'Mi nombre es {name} y tengo {age} años de edad.';
-        $actual   = Text::format($format, ['age' => 61, 'name' => 'Juan']);
-        $this->assertEquals($expected, $actual);
-
-        $format = 'Mi nombre es {1} y tengo {0} años de edad.';
-        $actual = Text::format($format, [61, 'Juan']);
-        $this->assertEquals($expected, $actual);
-
-        $actual = Text::format($format, 61, 'Juan');
-        $this->assertEquals($expected, $actual);
-
-        $expected = 'Números: 1, 2, 3, 4, 5, 6, 7, 8, 9 y 10.';
-        $format   = 'Números: ';
-        for ($i = 0; $i < 8; $i++) {
-            $format .= "{{$i}}, ";
-        }
-        $format .= '{8} y {9}.';
-        $actual = Text::format($format, range(1, 10));
-        $this->assertEquals($expected, $actual);
-    }
-
     /**
-     * @depends testPerformsFormatWithManyData
+     * @dataProvider validPositionalArgsListProvider
+     * @dataProvider validNamedArgsListProvider
      */
-    public function testPerformsFormatIgnoringNotMatchingData()
+    public function testPerformsFormatWithSecuentialAndNotSecuentialData($expected, $format, $data, $positional = false)
     {
-        $expected = 'Mi nombre es {name} y tengo {age} años de edad.';
-        $actual = Text::format('Mi nombre es {name} y tengo {age} años de edad.', ['fake_name' => 'Juan']);
+        $actual = Text::format($format, $data);
         $this->assertEquals($expected, $actual);
 
-        $actual = Text::format('Mi nombre es {name} y tengo {age} años de edad.', null);
-        $this->assertEquals($expected, $actual);
-
-        $actual = Text::format('Mi nombre es {name} y tengo {age} años de edad.', null, null, null);
-        $this->assertEquals($expected, $actual);
-
-        $expected = 'Mi nombre es Juan y tengo {age} años de edad.';
-        $actual = Text::format('Mi nombre es {name} y tengo {age} años de edad.', ['name' => 'Juan']);
-        $this->assertEquals($expected, $actual);
-
-        $expected = 'Mi nombre es {name} y tengo 54 años de edad.';
-        $actual = Text::format('Mi nombre es {name} y tengo {age} años de edad.', ['age' => 54]);
-        $this->assertEquals($expected, $actual);
+        if ($positional) {
+            $actual = Text::format($format, ...$data);
+            $this->assertEquals($expected, $actual);
+        }
     }
 
-    public function testPerformsFormatWithArgumentsOfDifferentTypesConvertiblesToString()
-    {
-        $expected = 'Invalid argument type. "major" (position 0) must to be an instance of "integer"; "string" given.';
-
-        $major = 'Non Number';
-        $args = [
-            'name'     => 'major',
-            'expected' => NML\typeof(0),
-            'pos'      => 0,
-            'actual' => NML\typeof($major),
-        ];
-
-        // Esto produce un error fatal debido a una debilidad en el uso de la función `asort` en el método
-        //  ``Cake\Utility\Text::.insert()``, ya que debería comparar como cadena usando el flag `SORT_STRING`
-        //  pero que se evita convirtiendo los valores a string en el método Text::format().
-
-        $actual = Text::format('Invalid argument type.', null);
-        $actual .= Text::format(
-            ' "{name}" (position {pos}) must to be an instance of "{expected}"; "{actual}" given.',
-            $args
-        );
-
-        $this->assertEquals($expected, $actual);
-    }
 
     /**
      * @expectedException InvalidArgumentException
@@ -146,9 +56,7 @@ class StringTest extends TestCase
      */
     public function testDoNotPerformsFormatWithPlaceholdersValuesNotConvertiblesToString($obj)
     {
-        $format = PHP_EOL.'This test should throws an "{0}" with data: {testErrorData}.'.PHP_EOL;
-
-        $str = Text::format($format, [InvalidArgumentException::class, 'testErrorData' => $obj]);
+        Text::format('{0}: {1}', InvalidArgumentException::class, $obj);
     }
 
     public function nonStringObjectsProvider()
@@ -157,6 +65,120 @@ class StringTest extends TestCase
             'stdClass' => [new \stdClass],
             'int[]'    => [[10, 20, 30, 40]],
             'string[]' => [['ten', '20', '30', '40']],
+        ];
+    }
+
+    /**
+     * expected, format, data, secuential
+     **/
+    public function validPositionalArgsListProvider()
+    {
+        $secuential = true;
+
+        return [
+            's: complete: basic array' => [
+                'Bob is 65 years old and has 101 cats.',
+                '{0} is {1} years old and has {2} cats.',
+                ['Bob', 65, 101],
+                $secuential,
+            ],
+            's: complete: basic array with extra data not in placeholder' => [
+                'Bob is 65 years old and has 101 cats.',
+                '{0} is {1} years old and has {2} cats.',
+                ['Bob', 65, 101, 'I am not here'],
+                $secuential,
+            ],
+            's: missing value for placeholder' => [
+                'Bob is 65 years old and has {2} cats.',
+                '{0} is {1} years old and has {2} cats.',
+                ['Bob', 65],
+                $secuential,
+            ],
+            's: with some empty data' => [
+                'Bob is 65 years old and has  cats.',
+                '{0} is {1} years old and has {2} cats.',
+                ['Bob', 65, ''],
+                $secuential,
+            ],
+            's: with some null data' => [
+                'Bob is 65 years old and has  cats.',
+                '{0} is {1} years old and has {2} cats.',
+                ['Bob', 65, null],
+                $secuential,
+            ],
+            's: only 1 argument null' => [
+                'Null is .',
+                'Null is {0}.',
+                [null],
+                $secuential,
+            ],
+            's: class implementing IConvertibleToString' => [
+                'x = (-1, 1)',
+                '{0} = ({1})',
+                ['x', new ClassString()],
+                $secuential
+            ],
+        ];
+    }
+
+    /**
+     * expected, format, data, secuential = false
+     **/
+    public function validNamedArgsListProvider()
+    {
+        return [
+            'n: complete' => [
+                'Bob is 65 years old and has 101 cats.',
+                '{name} is {age} years old and has {n} cats.',
+                [
+                    'name' => 'Bob',
+                    'age'     => 65,
+                    'n'    => 101
+                ],
+            ],
+            'n: complete with numeric index' => [
+                'Bob is 65 years old and has 101 cats.',
+                '{name} is {age} years old and has {7} cats.',
+                [
+                    'name' => 'Bob',
+                    'age'  => 65,
+                    7      => 101
+                ],
+            ],
+            'n: missing value for placeholder' => [
+                'Bob is 65 years old and has {n} cats.',
+                '{name} is {age} years old and has {n} cats.',
+                [
+                    'name' => 'Bob',
+                    'age'  => 65,
+                ],
+            ],
+            'n: complete with some empty value' => [
+                'Bob is 65 years old and has  cats.',
+                '{name} is {age} years old and has {n} cats.',
+                [
+                    'name' => 'Bob',
+                    'age'  => 65,
+                    'n'    => ''
+                ],
+            ],
+            'n: complete with some null values' => [
+                'Bob is 65 years old and has  cats.',
+                '{name} is {age} years old and has {n} cats.',
+                [
+                    'name' => 'Bob',
+                    'age'  => 65,
+                    'n'    => null
+                ],
+            ],
+            'n: class implementing IConvertibleToString' => [
+                'x = (-1, 1)',
+                '{var} = ({coords})',
+                [
+                    'var'    => 'x',
+                    'coords' => new ClassString(),
+                ],
+            ],
         ];
     }
 }
