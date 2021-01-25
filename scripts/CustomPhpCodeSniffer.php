84100ef799c26cba6158f389407f54b9118cbf9b
Nelson Martell <nelson6e65@gmail.com>
2021-01-25 13:19:38 -0500

    chore(composer): improve CustomPhpCodeSniffer script
    - Fix a bug when length of input path is too short (<4).
    - Improve messages.
    - Ignore not-found paths.

--- scripts/CustomPhpCodeSniffer.php
+++ scripts/CustomPhpCodeSniffer.php
@@ -31,35 +31,45 @@ class CustomPhpCodeSniffer
         $files = $event->getArguments();
         $count = count($files);
 
-        if ($count > 0) {
-            $event->getIO()->write('<options=bold>Checking Coding Standard of PHP files</>.');
+        $ignoredPaths = [];
 
-            $output = [];
-            $return = 0;
+        if ($count > 0) {
+            $event->getIO()->write("Checking PHP Coding Standard of ${count} paths.");
 
             foreach ($files as $i => $file) {
                 $realPath = realpath($file);
+
+                if (!$realPath) {
+                    $ignoredPaths[] = $file;
+                    continue;
+                }
 
                 if ($realPath === realpath(__FILE__)) {
                     // Do not self-fix this file
                     continue;
                 }
 
-                $relativeFile =  str_replace($rootDir . DIRECTORY_SEPARATOR, '', $realPath);
-                $relativeFile =  str_replace(DIRECTORY_SEPARATOR, '/', $relativeFile);
+                $relativePath =  str_replace(
+                    [$rootDir . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR],
+                    ['', '/'],
+                    $realPath
+                );
 
-                $type = stripos($relativeFile, '.php', -4) === false ? 'directory' : 'file';
+                $type = strlen($relativePath) < 4 || stripos($relativePath, '.php', -4) === false ? 'directory' : 'file';
 
-                $event->getIO()->write("Improving <options=bold>${relativeFile}</> ${type}...");
+                $event->getIO()->write("Improving <info>${relativePath}</info> ${type}...");
+
+                $output = [];
+                $return = 0;
 
                 // NOTE: workarround: need to run 2 times due to a bug that exits 1 instead of 0 when a file gets fixed
-                exec("${cmd} \"${relativeFile}\" || ${cmd} \"${relativeFile}\" -q", $output, $return);
+                exec("${cmd} \"${realPath}\" || ${cmd} \"${realPath}\" -q", $output, $return);
 
                 $event->getIO()->write($output, true, IOInterface::VERBOSE);
 
                 if ($return !== 0) {
-                    $event->getIO()->error("Error! Unable to autofix the ${relativeFile} file!");
-                    $event->getIO()->write('Run `composer cs:lint` to check manually the conflicting files');
+                    $event->getIO()->error("Error! Unable to autofix the ${relativePath} file!");
+                    $event->getIO()->write('<comment>Run <options=bold>`composer cs:lint`</> to check manually the conflicting files</comment>');
                     exit(1);
                 }
             }
@@ -71,5 +81,14 @@ class CustomPhpCodeSniffer
         $execution_time = round($end_time - $start_time, 2);
 
         $event->getIO()->write("Done in ${execution_time}s");
+
+        if (count($ignoredPaths)) {
+            $ignoredPaths = array_map(function ($item) {
+                return '  - ' . $item;
+            }, $ignoredPaths);
+
+            $event->getIO()->write('<comment>Note: Some paths were not found:</comment>');
+            $event->getIO()->write($ignoredPaths);
+        }
     }
 }
